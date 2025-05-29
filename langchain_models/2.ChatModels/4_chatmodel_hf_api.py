0b82184fb87079211a4e2ec804db33e20eb6da19
Kartik-Walia <itzkartikeds3@gmail.com>
2025-05-30 02:42:04 +0530

    Resolved Issue in running open-source chat model using HF Inference API (TinyLLM model isn't hosted on HuggingFace now)

--- langchain_models/2.ChatModels/4_chatmodel_hf_api.py
+++ langchain_models/2.ChatModels/4_chatmodel_hf_api.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0", 
+    repo_id="mistralai/Mistral-7B-Instruct-v0.3", 
     task="text-generation"
 )
 
