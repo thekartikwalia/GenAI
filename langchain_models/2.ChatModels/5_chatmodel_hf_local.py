ed7ec8b0c4359b37bb89618ae6c23ff011f8ce41
Kartik-Walia <itzkartikeds3@gmail.com>
2025-05-30 02:11:25 +0530

    Resolved Issue in running open-source chat model locally (Didn't initialised ChatHuggingFace properly & it didn't know about our HuggingFacePipeline, so it was trying to download some tokenizer to tokenize it)

--- langchain_models/2.ChatModels/5_chatmodel_hf_local.py
+++ langchain_models/2.ChatModels/5_chatmodel_hf_local.py
@@ -4,11 +4,11 @@ llm = HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     task="text-generation",
     pipeline_kwargs=dict(
-        temperature=0.5,
+        # temperature=0.5,
         max_new_tokens=100
     )
 )
-model = ChatHuggingFace(llm=llm)
+model = ChatHuggingFace(llm=llm, model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
 result = model.invoke("What is the capital of India")
 
